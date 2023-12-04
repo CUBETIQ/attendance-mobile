a3cf0ba72bf1a09f2d8795ddbe74e12fa546a032
sonphumrin <phumrin@cubetiqs.com>
2023-12-04 08:40:51 +0700

    update

---
+++ lib/feature/home/view/tablet/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeViewTablet extends StatelessWidget {
+  const HomeViewTablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

be9a404e6e8c184ed94172199d3cd1fae4683dc3
sonphumrin <phumrin@cubetiqs.com>
2024-02-27 17:15:45 +0700

    done upload service and add file util

--- lib/core/network/file_upload/upload_file_service.dart
+++ lib/core/network/file_upload/upload_file_service.dart
@@ -1,36 +1,28 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
 import 'package:timesync360/config/app_config.dart';
 import 'package:timesync360/core/network/dio/dio_util.dart';
+import 'package:timesync360/core/network/file_upload/model/file_metadata.dart';
 import 'package:timesync360/core/network/file_upload/model/upload_file_model.dart';
 import 'package:timesync360/utils/file_util.dart';
+import 'package:timesync360/utils/logger.dart';
 
 class UploadFileService {
+  static final _singleton = UploadFileService._internal();
   final dioInstance = DioUtil();
 
-  UploadFileService._();
+  factory UploadFileService() {
+    return _singleton;
+  }
 
-  static final UploadFileService _instance = UploadFileService._();
+  UploadFileService._internal() {
+    Logs.t('[UploadFileService] Initialized');
+  }
 
-  static UploadFileService get instance => _instance;
-
-  Future<UploadFileModel> uploadFile(File file) async {
+  Future<UploadFileModel?> uploadFile(File file, FileMetadata? metadata) async {
     try {
       dioInstance.setBaseUrl(AppConfig.uploadUrl);
-      final fileName = FileUtil.getFileName(file);
-      final contentType =
-          lookupMimeType(file.path) ?? 'application/octet-stream';
-      final data = FormData.fromMap(
-        {
-          "file": await MultipartFile.fromFile(
-            file.path,
-            filename: fileName,
-            contentType: MediaType.parse(contentType),
-          ),
-        },
-      );
+      final data = FileUtil.getFormData(file, metadata);
       final response = await dioInstance.dio.post(
         "/upload",
         data: data,
@@ -40,8 +32,9 @@ class UploadFileService {
       } else {
         throw Exception("Upload file failed");
       }
-    } catch (e) {
-      rethrow;
+    } on DioException catch (e) {
+      Logs.e(e.message);
     }
+    return null;
   }
 }

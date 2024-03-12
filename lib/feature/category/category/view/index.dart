ce2c14d656d5e52f92c54e2df92b04ef9de162de
kit <kit@cubetiqs.com>
2024-03-12 11:12:55 +0700

    update category screen

--- lib/feature/category/category/view/index.dart
+++ lib/feature/category/category/view/index.dart
@@ -1,15 +1,70 @@
-import 'package:timesync/core/widgets/layout_builder/responsive_layout.dart';
 import 'package:flutter/material.dart';
-import 'package:timesync/feature/category/category/view/mobile/index.dart';
-import 'package:timesync/feature/category/category/view/tablet/index.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:timesync/core/widgets/button/back_button.dart';
+import 'package:timesync/core/widgets/pull_refresh/refresh_indicator.dart';
+import 'package:timesync/core/widgets/text/app_bar_title.dart';
+import 'package:timesync/feature/category/category/controller/index.dart';
+import 'package:timesync/feature/category/category/widget/category_card.dart';
+import 'package:timesync/utils/size_util.dart';
 
 class CategoryView extends StatelessWidget {
   const CategoryView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const ResponsiveLayout(
-      tablet: CategoryViewTablet(),
-      mobile: CategoryViewMobile(),
+    final controller = CategoryController.to;
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const MyAppBarTitle(
+          title: "Category",
+        ),
+        centerTitle: true,
+        leading: const MyBackButton(),
+        automaticallyImplyLeading: false,
+      ),
+      body: MyRefreshIndicator(
+        onRefresh: controller.onRefresh,
+        child: SizedBox(
+          width: size.width,
+          height: size.height,
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: SizeUtils.scale(10, size.width),
+                horizontal: SizeUtils.scale(24, size.width),
+              ),
+              child: Obx(
+                () => Wrap(
+                  spacing: SizeUtils.scale(8, size.width),
+                  alignment: WrapAlignment.start,
+                  children: [
+                    ...List.generate(
+                      controller.categories.length,
+                      (index) => CategoryCard(
+                        onTap: () => controller.onTapCategory(
+                          controller.categories[index],
+                        ),
+                        category: controller.categories[index],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.onTapAddCategory,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+        ),
+        child: const Icon(Icons.add_rounded),
+      ),
     );
   }
 }

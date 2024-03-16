38c31b7c6034d3a0ddd5ccd26d791741227c9fa0
sonphumrin <phumrin@cubetiqs.com>
2024-03-16 08:51:30 +0700

    fix error on employee deatil view when position and department is null

--- lib/feature/employee/employee_detail/controller/index.dart
+++ lib/feature/employee/employee_detail/controller/index.dart
@@ -2,11 +2,12 @@ import 'package:timesync/core/model/department_model.dart';
 import 'package:timesync/core/model/position_model.dart';
 import 'package:timesync/core/model/user_model.dart';
 import 'package:get/get.dart';
+import 'package:timesync/utils/logger.dart';
 
 class StaffDetailController extends GetxController {
   final staff = UserModel().obs;
-  final position = PositionModel().obs;
-  final department = DepartmentModel().obs;
+  final position = Rxn<PositionModel>(null);
+  final department = Rxn<DepartmentModel>(null);
 
   @override
   void onInit() {
@@ -14,5 +15,11 @@ class StaffDetailController extends GetxController {
     staff.value = Get.arguments['staff'];
     position.value = Get.arguments['position'];
     department.value = Get.arguments['department'];
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    Logs.i("StaffDetailController onClose");
   }
 }

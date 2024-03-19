8984bd502d2718beb90ddd3bfb2bc15f7e1e380d
Sambo Chea <sombochea@cubetiqs.com>
2024-03-19 08:13:22 +0700

    Add keys and updated ios build config

--- lib/key.dart
+++ lib/key.dart
@@ -7,6 +7,11 @@ class Key {
 
   Key._internal();
 
-  static const String publicKey = '25882306345b5b05ea796bc421b15b62';
-  static const String xApiKey = 'fb73ce111f0ddde0e56569aa31fc8d30';
+  // DEV
+  static const String vfsClientPublicKey = '25882306345b5b05ea796bc421b15b62';
+  static const String vfsClientApiKey = 'fb73ce111f0ddde0e56569aa31fc8d30';
+
+  // PROD
+  // static const String vfsClientPublicKey = '577553517a76666b4e6a4f65673d0a';
+  // static const String vfsClientApiKey = '444b2b6e6356584d37736134513d0a';
 }

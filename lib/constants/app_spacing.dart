85a6c83364a0ab87ed131faa8bc7e0e2c80dde0f
sonphumrin <phumrin@cubetiqs.com>
2024-03-21 08:39:18 +0700

    update

---
+++ lib/constants/app_spacing.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: constant_identifier_names
+
+class AppSpacing {
+  static final AppSpacing _instance = AppSpacing._internal();
+
+  factory AppSpacing() {
+    return _instance;
+  }
+
+  AppSpacing._internal();
+
+  static const double XS4 = 1.0;
+  static const double XS3 = 2.0;
+  static const double XS2 = 4.0;
+  static const double XS = 6.0;
+  static const double S = 8.0;
+  static const double M = 12.0;
+  static const double L = 16.0;
+  static const double XL = 20.0;
+  static const double XL2 = 24.0;
+  static const double XL3 = 32.0;
+  static const double XL4 = 40.0;
+  static const double XL5 = 46.0;
+}

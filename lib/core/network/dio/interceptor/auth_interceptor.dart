62b5c6d5ddf7c03f0fcd98278199c4eb3c9d81c3
sonphumrin <phumrin@cubetiqs.com>
2024-03-21 16:30:22 +0700

    add header x-organization-id for validate

--- lib/core/network/dio/interceptor/auth_interceptor.dart
+++ lib/core/network/dio/interceptor/auth_interceptor.dart
@@ -1,6 +1,7 @@
 import 'package:timesync/config/app_config.dart';
 import 'package:timesync/core/database/isar/controller/local_storage_controller.dart';
 import 'package:dio/dio.dart';
+import 'package:timesync/utils/logger.dart';
 
 class AuthInterceptor extends Interceptor {
   @override
@@ -14,13 +15,18 @@ class AuthInterceptor extends Interceptor {
         'content-type': 'application/json',
         'Authorization': "Bearer ${localData?.accessToken}",
         'User-Agent': AppConfig.getUserAgent,
+        'x-organization-id':
+            AppConfig.getLocalData?.organizationId ?? "default",
       });
     } else {
       options.headers.addAll({
         'content-type': 'application/json',
         'User-Agent': AppConfig.getUserAgent,
+        'x-organization-id':
+            AppConfig.getLocalData?.organizationId ?? "default",
       });
     }
+    Logs.i(options.headers.toString());
     return super.onRequest(options, handler);
   }
 }

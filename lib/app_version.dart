16da54c5391908069500626ae817dadaf09be5b3
Sambo Chea <sombochea@cubetiqs.com>
2024-03-22 21:50:14 +0700

    Add changelog and upgraded to 1.0.0+5

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
   // version format: {major}{minor}{patch}.{build}
-  static const double version = 100.4;
+  static const double version = 100.5;
 
   // static double? get iosVersion => AppConfig.metaData?.appAndroidVersion;
 

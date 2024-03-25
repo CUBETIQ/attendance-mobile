60d4b1287ee4a24df0e94d5f7c9244d2c5a031cb
Sambo Chea <sombochea@cubetiqs.com>
2024-03-25 21:03:50 +0700

    Upgraded to 1.0.0+7

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
   // version format: {major}{minor}{patch}.{build}
-  static const double version = 100.6;
+  static const double version = 100.7;
 
   // static double? get iosVersion => AppConfig.metaData?.appAndroidVersion;
 

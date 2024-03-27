a643637102cf4dc5a0923d657f59583ec9426d93
sonphumrin <phumrin@cubetiqs.com>
2024-03-27 17:21:42 +0700

    update

--- lib/feature/leave/leave/view/index.dart
+++ lib/feature/leave/leave/view/index.dart
@@ -12,7 +12,7 @@ import 'package:timesync/core/widgets/text/text.dart';
 import 'package:timesync/extensions/string.dart';
 import 'package:timesync/feature/leave/leave/controller/index.dart';
 import 'package:flutter/material.dart';
-import 'package:timesync/feature/leave/leave/widget/new_leave_card.dart';
+import 'package:timesync/feature/leave/leave/widget/leave_card.dart';
 import 'package:timesync/utils/size_util.dart';
 
 class LeaveView extends StatelessWidget {
@@ -141,9 +141,9 @@ class LeaveView extends StatelessWidget {
                             controller.leaves[index],
                           ),
                           leave: controller.leaves[index],
-                          // onTapView: () => controller.onTapView(index),
-                          // onTapCancel: () =>
-                          //     controller.onTapCancel(controller.leaves[index]),
+                          onTapView: () => controller.onTapView(index),
+                          onTapCancel: () =>
+                              controller.onTapCancel(controller.leaves[index]),
                         );
                       },
                     ),
